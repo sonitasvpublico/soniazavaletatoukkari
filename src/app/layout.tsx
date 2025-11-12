231927a8e2892f3c5a95658eaaedfd4dea8e4ba4
Sonia Maria Zavaleta Toukkari <sonia_z_14@hotmail.com>
2025-11-12 18:21:48 +0000

    ok entonces como hacemos con lo del favicon? cual es la solucion mas via

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,6 @@ import { Toaster } from "@/components/ui/toaster";
 export const metadata: Metadata = {
   title: 'Sonia Zavaleta Toukkari',
   description: 'QA Specialist & UX/UI Expert. Crafting flawless digital experiences.',
-  icons: {
-    icon: 'https://i.stack.imgur.com/3o83k.png',
-  },
 };
 
 export default function RootLayout({
@@ -18,7 +15,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="https://i.stack.imgur.com/3o83k.png" sizes="any" />
+        <link rel="icon" href="https://i.stack.imgur.com/3o83k.png" type="image/png" sizes="32x32" />
+        <link rel="icon" href="https://i.stack.imgur.com/3o83k.png" type="image/png" sizes="16x16" />
+        <link rel="apple-touch-icon" href="https://i.stack.imgur.com/3o83k.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&family=Poiret+One&display=swap" rel="stylesheet" />
