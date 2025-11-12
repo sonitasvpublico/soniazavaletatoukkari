2e9c14e027a40dfb3621383d7b0af82ea90d7e78
Sonia Maria Zavaleta Toukkari <sonia_z_14@hotmail.com>
2025-11-12 09:22:18 +0000

    podrias poner esa foto en vez de la muchacha?

--- next.config.ts
+++ next.config.ts
@@ -33,6 +33,12 @@ const nextConfig: NextConfig = {
         hostname: 'i.stack.imgur.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
