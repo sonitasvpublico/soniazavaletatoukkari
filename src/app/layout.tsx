d5ce0d510e7ec6c7ff48ade70a94357d43f7de17
Sonia Maria Zavaleta Toukkari <sonia_z_14@hotmail.com>
2025-11-12 17:32:26 +0000

    Quiero que en vez de SonitaSV Enhanced portafolio se llame Sonia Zavalet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
-  title: 'SonitaSV Enhanced Portfolio',
+  title: 'Sonia Zavaleta Toukkari',
   description: 'QA Specialist & UX/UI Expert. Crafting flawless digital experiences.',
 };
 
@@ -18,6 +18,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&family=Poiret+One&display=swap" rel="stylesheet" />
+        <link rel="icon" href="https://sonitasv.com/wp-content/uploads/2024/02/cropped-sv-logo-32x32.png" sizes="any" />
       </head>
       <body className="font-body antialiased">
         {children}
