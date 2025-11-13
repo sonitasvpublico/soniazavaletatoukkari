a6b205f9c50aef3ab4fa85d61d2bab91bbf7c9d7
Sonia Maria Zavaleta Toukkari <sonia_z_14@hotmail.com>
2025-11-13 10:26:36 +0000

    Fijate que Software Developer | UX/UI & QA se ven 3 lineas en mobile, po

--- src/components/page/header.tsx
+++ src/components/page/header.tsx
@@ -3,7 +3,7 @@ export default function Header() {
   return (
     <header className="mb-12 flex flex-col items-center justify-center gap-8">
       <div className="text-center">
-        <h1 className="font-headline text-4xl font-bold md:text-5xl lg:text-6xl drop-shadow-lg">
+        <h1 className="font-headline text-3xl font-bold md:text-5xl lg:text-6xl drop-shadow-lg">
           Software Developer | UX/UI & QA
         </h1>
         <p className="mt-4 max-w-3xl text-lg md:text-xl drop-shadow">
