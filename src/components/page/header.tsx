0ccae7ba43bb4b96d3d6fa9d4f4bde6e90f710b5
Sonia Maria Zavaleta Toukkari <sonia_z_14@hotmail.com>
2025-11-24 08:46:32 +0000

    Building high-quality digital experiences with a focus on clean code, gr

--- src/components/page/header.tsx
+++ src/components/page/header.tsx
@@ -1,12 +1,12 @@
 
 export default function Header() {
   return (
-    <header className="mb-8 flex flex-col items-center justify-center gap-8 md:mb-12">
+    <header className="mb-8 flex flex-col items-center justify-center gap-8 md:mb-8">
       <div className="text-center">
         <h1 className="font-headline text-3xl font-bold md:text-5xl lg:text-6xl drop-shadow-lg">
           Software Developer | QA & UX/UI
         </h1>
-        <p className="mt-4 max-w-3xl text-lg md:text-xl drop-shadow">
+        <p className="mt-4 max-w-3xl text-lg md:max-w-full md:text-xl lg:text-2xl drop-shadow">
           Building high-quality digital experiences with a focus on clean code, great usability, and rigorous testing.
         </p>
       </div>
