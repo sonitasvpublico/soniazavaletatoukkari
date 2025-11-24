3dd478c28c1cae9ee7bb52e4fc4192cf1405a71c
Sonia Maria Zavaleta Toukkari <sonia_z_14@hotmail.com>
2025-11-24 08:48:41 +0000

    revertilo, me jodiste los botones y las otras versiones, dejalo como est

--- src/components/page/header.tsx
+++ src/components/page/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
         <h1 className="font-headline text-3xl font-bold md:text-5xl lg:text-6xl drop-shadow-lg">
           Software Developer | QA & UX/UI
         </h1>
-        <p className="mt-4 max-w-3xl text-lg md:max-w-full md:text-xl lg:text-2xl drop-shadow">
+        <p className="mt-4 max-w-3xl text-lg md:text-xl drop-shadow">
           Building high-quality digital experiences with a focus on clean code, great usability, and rigorous testing.
         </p>
       </div>
