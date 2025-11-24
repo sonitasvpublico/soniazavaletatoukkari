fed8b581e77e8341da5c607c19f22a518554734a
Sonia Maria Zavaleta Toukkari <sonia_z_14@hotmail.com>
2025-11-24 08:12:05 +0000

    esq no se, la verdad esq estoy confundida, pudiera ser Software Develope

--- src/components/page/header.tsx
+++ src/components/page/header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
     <header className="mb-12 flex flex-col items-center justify-center gap-8">
       <div className="text-center">
         <h1 className="font-headline text-3xl font-bold md:text-5xl lg:text-6xl drop-shadow-lg">
-          Software Developer | UX/UI & QA
+          Software Developer | QA & UX/UI
         </h1>
         <p className="mt-4 max-w-3xl text-lg md:text-xl drop-shadow">
           Building high-quality digital experiences with a focus on clean code, great usability, and rigorous testing.
